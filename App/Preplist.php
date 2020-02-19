84d48461d96021801911139a3126b338b007b1b1
David <dave312@hotmail.co.uk>
2020-02-19 19:43:32 +0000

    ud

--- App/Preplist.php
+++ App/Preplist.php
@@ -24,7 +24,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <link  rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css'>
   <script src="JS/Preplist.js"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-
   <script type="text/javascript" src="script.js"></script>
 
 </head>
@@ -53,7 +52,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
          <div class="item4">
            <div class="fourth">
              <h3>Preparation List</h3>
-
              <ul>
                <li class="draggable" id="draggable" draggable="true">Food goes here</li>
               <li class="draggable"id="draggable" draggable="true">Sauce</li>
@@ -65,17 +63,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
          <div class="item5">
          <div class="fifth">
            <h3>Search Components</h3>
-
            <div class="adder">
              <div class="search-box">
              <input type="text" id="search" class="input" autocomplete="off" placeholder="Add items in your list..">
              <div id="display"></div>
              <span class="add">+</span>
-              <div class="result"></div>
+
            </div>
          </div>
-
-         <!-- Suggestions will be displayed in below div. -->
          </div>
       </div>
 
@@ -99,31 +94,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
   <script src="JS/pageup.js"></script>
   <script src="JS/Preplist.js"></script>
-
-  <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-    $('.search-box input[type="text"]').on("keyup input", function(){
-        /* Get input value on change */
-        var inputVal = $(this).val();
-        var resultDropdown = $(this).siblings(".result");
-        if(inputVal.length){
-            $.get("PHP/preplist.php", {term: inputVal}).done(function(data){
-                // Display the returned data in browser
-                resultDropdown.html(data);
-            });
-        } else{
-            resultDropdown.empty();
-        }
-    });
-
-    // Set search input value on click of result item
-    $(document).on("click", ".result p", function(){
-        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
-        $(this).parent(".result").empty();
-    });
-});
-</script>
 </footer>
 
 </html>
