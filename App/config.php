c8810e2e3c5d3df01e018c3818e5ec99a7be01a0
David <dave312@hotmail.co.uk>
2020-04-26 13:39:58 +0100

    version 3.0
    Final update

--- App/config.php
+++ App/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'gretchen_David');
-define('DB_PASSWORD', 'YxJ2Zbfw)zH+');
-define('DB_NAME', 'gretchen_kmt');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'kmt');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
